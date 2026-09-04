28c4d506cf39be32a55c0bcc34601f84b2d7c88c
anargihu <ndjd86d5fw@privaterelay.appleid.com>
2026-09-04 14:12:11 +0800

    Create Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace OurSigner;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Title = "OurSigner";
+        Console.WriteLine("OurSigner");
+        Console.WriteLine("One-time Windows setup");
+        Console.WriteLine();
+        Console.WriteLine("Setup helper started.");
+        Console.WriteLine("Press Enter to exit.");
+        Console.ReadLine();
+    }
+}
